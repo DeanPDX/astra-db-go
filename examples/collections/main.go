9f2c9f29960ee2eb70d37cc7eafdefa30ef1f20b
Dean Davidson <deand@sembit.com>
2026-03-04 14:49:10 -0800

    Beef up README/Examples (#11)
    * Change collection to be more consistent
    
    * Update readme and add examples
    
    * Get full example docs
    
    * Add comment about builder pattern
    
    * Remove nil options
    
    * Cursor: check for err when using All
    
    * Clean up examples
    
    * Add plug for examples
    
    * Update examples/README.md
    
    Co-authored-by: toptobes <96998732+toptobes@users.noreply.github.com>
    
    * Change comment to match
    
    * Break examples up
    
    ---------
    
    Co-authored-by: toptobes <96998732+toptobes@users.noreply.github.com>

---
+++ examples/collections/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/DeanPDX/dotconfig"
+	astradb "github.com/datastax/astra-db-go"
+	"github.com/datastax/astra-db-go/filter"
+	"github.com/datastax/astra-db-go/options"
+)
+
+// Config is a struct for retrieving configuration from environment variables.
+// The `env` tags specify the environment variable names to look for when
+// populating this struct. If you have the [Astra CLI] installed, you can run
+// the following command to generate a .env file with the necessary variables:
+//
+//	astra db create-dotenv mydatabase
+//
+// [Astra CLI]: https://docs.datastax.com/en/astra-cli/index.html
+type Config struct {
+	DBEndpoint       string `env:"ASTRA_DB_API_ENDPOINT"`
+	ApplicationToken string `env:"ASTRA_DB_APPLICATION_TOKEN"`
+}
+
+// Book represents a document in the collection.
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   int    `json:"year"`
+}
+
+func main() {
+	// Load our configuration and create a client and DB handle.
+	config, err := dotconfig.FromFileName[Config](".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Configuration loaded successfully.")
+	client := astradb.NewClient(
+		options.WithToken(config.ApplicationToken),
+	)
+	db := client.Database(config.DBEndpoint)
+	fmt.Printf("Using database endpoint: %s\n", config.DBEndpoint)
+	ctx := context.Background()
+
+	coll := createCollection(ctx, db)
+	insertDocuments(ctx, coll)
+	findOneDocument(ctx, coll)
+	findAllDocuments(ctx, coll)
+	filterDocumentsByYear(ctx, coll)
+	countDocuments(ctx, coll)
+	dropCollection(ctx, db)
+}
+
+func createCollection(ctx context.Context, db *astradb.Db) *astradb.Collection {
+	logHeader("Creating Collection")
+	coll, err := db.CreateCollection(ctx, "my_collection")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Created collection: my_collection")
+	return coll
+}
+
+func insertDocuments(ctx context.Context, coll *astradb.Collection) {
+	logHeader("Inserting Documents")
+	resp, err := coll.InsertOne(ctx, Book{
+		Title:  "The Go Programming Language",
+		Author: "Alan Donovan",
+		Year:   2015,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted: \"The Go Programming Language\" by Alan Donovan (2015)")
+	fmt.Println("Inserted IDs", resp.Status.InsertedIds)
+
+	// Insert multiple documents
+	resp, err = coll.InsertMany(ctx, []Book{
+		{Title: "Go in Action", Author: "William Kennedy", Year: 2015},
+		{Title: "Concurrency in Go", Author: "Katherine Cox-Buday", Year: 2017},
+		{Title: "Learning Go: An Idiomatic Approach to Real-World Go Programming", Author: "Jon Bodner", Year: 2024},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Inserted %d more documents via InsertMany:\n", len(resp.Status.InsertedIds))
+	for _, id := range resp.Status.InsertedIds {
+		fmt.Printf("  - %s\n", id)
+	}
+}
+
+func findOneDocument(ctx context.Context, coll *astradb.Collection) {
+	logHeader("Finding: title = 'The Go Programming Language'")
+	var result Book
+	err := coll.FindOne(ctx, filter.Eq("title", "The Go Programming Language")).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Found: %s by %s (%d)\n", result.Title, result.Author, result.Year)
+}
+
+func findAllDocuments(ctx context.Context, coll *astradb.Collection) {
+	logHeader("Finding All Documents")
+	cursor := coll.Find(ctx, filter.F{})
+	defer cursor.Close(ctx)
+
+	var books []Book
+	if err := cursor.All(ctx, &books); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Found %d result(s):\n", len(books))
+	for _, b := range books {
+		fmt.Printf("  - %s (%d)\n", b.Title, b.Year)
+	}
+}
+
+func filterDocumentsByYear(ctx context.Context, coll *astradb.Collection) {
+	logHeader("Filtering: year >= 2016")
+	cursor := coll.Find(ctx, filter.F{"year": filter.F{"$gte": 2016}})
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var book Book
+		if err := cursor.Decode(&book); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("  - %s by %s (%d)\n", book.Title, book.Author, book.Year)
+	}
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func countDocuments(ctx context.Context, coll *astradb.Collection) {
+	logHeader("Counting Documents")
+	count, err := coll.CountDocuments(ctx, filter.F{}, 1000)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Total documents:", count)
+}
+
+func dropCollection(ctx context.Context, db *astradb.Db) {
+	logHeader("Dropping Collection")
+	if err := db.DropCollection(ctx, "my_collection"); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Collection dropped.")
+}
+
+func logHeader(title string) {
+	fmt.Printf("\n--- %s ---\n", title)
+}
